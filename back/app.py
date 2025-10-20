1c25265d5cac21703d85fa7660c2a2e4de8af5a4
= <=>
2025-10-20 22:15:25 +0900

    back 설정 수정

--- back/app.py
+++ back/app.py
@@ -184,5 +184,6 @@ def optimize_gif_endpoint():
     return jsonify({'results': results}), 200
 
 # ----------------- Server Run -----------------
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
